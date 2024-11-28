cd4e9db01d5fe1e5f0b5ee45cfeeee5a7816ffec
hmarmy <hmarmy@gmail.com>
2024-11-28 09:44:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
+
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto('https://developers.google.com/web/');
+
+  console.log(browser);
+})
+
+app.get("/", (req, res) => res.type('html').send(html));
 
 const html = `
 <!DOCTYPE html>
